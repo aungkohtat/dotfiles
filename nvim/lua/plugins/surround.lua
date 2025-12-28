66d06775788c27d60d1fe286fea0fe4cffce44a3
Aung Ko Htet <aung.kohtet@messagespring.com>
2025-12-28 14:20:33 +0700

    surround-name-change

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,14 +1,16 @@
 return {
-  "nvim-mini/mini.surround",
-  opts = {
-    mappings = {
-      add = "sa",
-      delete = "sd",
-      find = "gsf",
-      find_left = "gsF",
-      highlight = "gsh",
-      replace = "gsr",
-      update_n_lines = "gsn",
+  {
+    "nvim-mini/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
     },
   },
 }
